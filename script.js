a899d0c79d2811d540db9f72b674bccbd3f232a1
Tokelo Seremane <seremane.t@gmail.com>
2021-07-28 11:07:59 +0200

    A whole lot of changes were made newRecipe works

--- script.js
+++ script.js
@@ -52,36 +52,7 @@ function appendData(data) {
 
 //wite new recipe object to JSON
 
-// Example POST method implementation:
-async function postData(url = '', data = {}) {
-    // Default options are marked with *
-    const response = await fetch(url, {
-      method: 'POST', // *GET, POST, PUT, DELETE, etc.
-      //mode: 'cors', // no-cors, *cors, same-origin
-      //cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
-      //credentials: 'same-origin', // include, *same-origin, omit
-      headers: {
-        'Content-Type': 'application/json'
-        // 'Content-Type': 'application/x-www-form-urlencoded',
-      },
-      redirect: 'follow', // manual, *follow, error
-      referrerPolicy: 'no-referrer', // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
-      body: JSON.stringify(data) // body data type must match "Content-Type" header
-    });
-    return response.json(); // parses JSON response into native JavaScript objects
-  }
-  
-  postData('allRecipes', { title:"Choccake", 
-  recipe:{
-      ingredients:["cocoa","eggs","milk","flour"],
-      instructions:["Sieve flour","mix all ingredients","ensure that batter is smooth","cook untill bubbles"],
-      appliances:["bowl","whisk","cake pan"]
 
-  },
-  tag:"Dessert" })
-    .then(data => {
-      console.log(data); // JSON data parsed by `data.json()` call
-    });
   
 // var newData = { title:"Choccake", 
 //         recipe:{
@@ -96,40 +67,112 @@ async function postData(url = '', data = {}) {
 
 
 
-// class Recipes{
-//     constructor(title,ingredients,instructions,appliances){
-//         this.title = title;
-//         this.ingredients= ingredients;
-//         this.instructions= instructions;
-//         this.appliances= appliances;
-//     }
+class Recipes{
+    constructor(title,ingredients=[],instructions=[],appliances=[]){
+        this.title = title;
+        this.ingredients= ingredients;
+        this.instructions= instructions;
+        this.appliances= appliances;
+    }
 
-//     getTitle(){
-//         let displayTitle = this.title;
-//         return displayTitle;
-//     }
-//     getIngred(){
-//         let displayIngred = this.ingredients;
-//         return displayIngred;
-//     }
-//     getInstru(){
-//         let displayInstru = this.instructions;
-//         return displayInstru;
-//     }
-//     getApplian(){
-//         let displayApplian = this.appliances;
-//         return displayApplian;
-//     }   
-// }
+    getTitle(x){
+        let displayTitle = this.x;
+        return displayTitle;
+    }
+    getIngred(y){
+        let displayIngred = this.y;
+        return displayIngred;
+    }
+    getInstru(z){
+        let displayInstru = this.z;
+        return displayInstru;
+    }
+    getApplian(w){
+        let displayApplian = this.w;
+        return displayApplian;
+    }   
+}
+function newRec(t,i,s,a){
+    t= document.getElementById("ftitle").value;
+    i= document.getElementById("breakfastSelect").options[breakfastSelect.selectedIndex].text;
+    s= document.getElementById("textArea").value;
+     //split mytext by nextline = "\n" 
+     var myArray = s.split("\n");
 
-// function getData(){
+     //to check the result
+     var result = '';
+     for(i=0;i<myArray.length;i++)
+     {
+         result += myArray[i]+"<br>";
+     }
+ 
+     document.getElementById("result").innerHTML = result;
+    let ave= new Recipes(t,i,s,a);
+    return ave;
+}
+
+function textToArray() {
+    //object to contain data
     
-//     fetch("./allRecipes.JSON")
-//     .then(response => {
-//        return response.json();
-//     })
-//     .then(data => console.log(data));
-// }
+    //assign value of textarea to mytext
+    var myText = document.getElementById("textArea").value;
+    var mySteps = document.getElementById("StepsTextarea1").value;
+    var myAppli =document.getElementById("appliTextarea1").value
+    //split mytext by nextline = "\n" 
+    var myArray = myText.split("\n");
+    var myArray2 = mySteps.split("\n");
+    var myArray3 = myAppli.split("\n");
+    //to check the result
+    var result = '';
+    var arrList =[];
+    var arrSteps =[];
+    var arrAppli =[];
+    for(i=0;i<myArray.length;i++)
+    {
+        var temp ="";
+        temp = myArray[i];
+        result += myArray[i]+"<br>";
+        console.log("added Ingredients");
+        arrList[i]= temp;
+    }
+    for(k=0; k<myArray2.length;k++);{
+        var temp2 ="";
+        temp2 = myArray2[k];
+        result += myArray2[k]+"<br>";
+        console.log("added Instructions");
+        arrSteps[k]= temp2;
+    }
+    for(l=0; l<myArray3.length;l++);{
+        var temp3 ="";
+        temp3 = myArray3[l];
+        result += myArray3[l]+"<br>";
+        console.log("added Appliances");
+        arrAppli[l]= temp3;
+    }
+
+    document.getElementById("result").innerHTML = result;    
+    var recipMethod;
+    recipMethod = {
+        "ingredients":arrList ,
+        "instructions": arrSteps,
+        "appliances": arrAppli
+    };
+    return console.log(recipMethod);
+}
+
+function getname(){
+    let temp = document.getElementById("breakfastSelect").options[breakfastSelect.selectedIndex].text;
+    return console.log(temp);
+}
+//get json files data
+/*function getData(){
+    
+    fetch("./allRecipes.JSON")
+    .then(response => {
+       return response.json();
+    })
+    .then(data => console.log(data));
+}*/
 
 //display new Recipe 
 function getRecipe(){
@@ -144,7 +187,7 @@ function getRecipe(){
     // return displayRecipe;
 }
 
-
+//getname();
 console.log("Working");
 
 
